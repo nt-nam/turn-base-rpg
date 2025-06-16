20ec9889866024a5b9827f8b9bca308f1825b60d
Nam <95182414+Thanhman5@users.noreply.github.com>
2025-06-16 12:32:24 +0700

    save data

--- core/src/main/java/com/game/screens/start/NewPlayerScreen.java
+++ core/src/main/java/com/game/screens/start/NewPlayerScreen.java
@@ -3,7 +3,6 @@ package com.game.screens.start;
 import static com.game.utils.Constants.*;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
@@ -11,14 +10,23 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Array;
 import com.game.MainGame;
 import com.game.screens.BaseScreen;
 import com.game.screens.ScreenType;
 import com.game.ui.base.UIButton;
 import com.game.ui.base.UIImage;
+import com.game.ui.base.UILabel;
 
 public class NewPlayerScreen extends BaseScreen {
+
+    private int currentKnightIndex = 0;
+    private final Array<String> knightIds = new Array<>();
+    private Image knightAnimImage; // actor animation
+    private String[] knightNames = { "Knight A", "Knight B", "Knight C", "Knight D", "Knight E", "Knight F", "Knight G", "Knight H", "Knight I", "Knight J" }; // tuỳ bạn
+    private UILabel knightNameLabel; // Label tên knight
+    private UILabel knightStatsLabel; // Label mô tả/thông số
 
     public NewPlayerScreen() {
         super();
@@ -39,27 +47,118 @@ public class NewPlayerScreen extends BaseScreen {
         Gdx.app.log("NewPlayerScreen", "show() called");
         createBackground();
         createTitle("New Player Screen", rootGroup);
-        TextureRegion skill = MainGame.getAsM().getRegion(SKILL_SKILL, "blue1");
-        Image skillImage = new Image(skill);
-        skillImage.setPosition(100, 100);
-        rootGroup.addActor(skillImage);
 
-
-        for (int i = 1; i <= 2; i++) {
-            String knightId = String.format("%02dKnight", i);
-            createCharacter(CHARACTER + knightId + ".atlas","deal",i);
+        // Khởi tạo danh sách knightId
+        for (int i = 1; i <= 10; i++) {
+            knightIds.add(String.format("%02dKnight", i));
         }
-    }
 
+        // Khởi tạo image nhân vật (animation)
+        knightAnimImage = createCharacterImage(knightIds.get(currentKnightIndex), "idle");
+        knightAnimImage.setSize(screenHeight * 0.7f, screenHeight * 0.7f);
+        knightAnimImage.setPosition((screenWidth - knightAnimImage.getWidth()) / 2f, screenHeight * 0.3f);
+        rootGroup.addActor(knightAnimImage);
+
+        // Label tên nhân vật
+        knightNameLabel = new UILabel(knightNames[currentKnightIndex]);
+        knightNameLabel.setPosition(screenWidth / 2f, screenHeight * 0.22f, Align.center);
+        rootGroup.addActor(knightNameLabel);
+
+        // Label stats/mô tả (giả lập, thay bằng hàm lấy stats thực)
+        knightStatsLabel = new UILabel(getKnightStats(knightIds.get(currentKnightIndex)));
+        knightStatsLabel.setPosition(screenWidth / 2f, screenHeight * 0.16f, Align.center);
+        rootGroup.addActor(knightStatsLabel);
+
+        // Nút NEXT
+        UIButton btnNext = new UIButton(MainGame.getAsM().getRegion(UI_WOOD, "next_up_005"),
+            MainGame.getAsM().getRegion(UI_WOOD, "next_down_006"))
+            .size(screenHeight * 0.18f, screenHeight * 0.18f)
+            .pos(screenWidth * 0.80f, screenHeight * 0.42f)
+            .onClick(this::showNextKnight);
+        rootGroup.addActor(btnNext);
+
+        // Nút PREV
+        UIButton btnPrev = new UIButton(MainGame.getAsM().getRegion(UI_WOOD, "next_up_005"),
+            MainGame.getAsM().getRegion(UI_WOOD, "next_down_006"))
+            .size(screenHeight * 0.18f, screenHeight * 0.18f)
+            .pos(screenWidth * 0.02f, screenHeight * 0.42f)
+            .onClick(this::showPrevKnight);
+        rootGroup.addActor(btnPrev);
+
+        // Nút chọn knight
+        UIButton btnSelect = new UIButton("Chọn", "default")
+            .size(220, 70)
+            .pos(screenWidth/2f - 110, screenHeight * 0.07f)
+            .onClick(() -> {
+                String selectedKnight = knightIds.get(currentKnightIndex);
+                Gdx.app.log("KnightSelected", selectedKnight);
+                // Xử lý lưu chọn nhân vật tại đây
+            });
+        rootGroup.addActor(btnSelect);
+
+//        TextureRegion skill = MainGame.getAsM().getRegion(SKILL_SKILL, "blue1");
+//        Image skillImage = new Image(skill);
+//        skillImage.setPosition(100, 100);
+//        rootGroup.addActor(skillImage);
+//
+//
+//        for (int i = 1; i <= 2; i++) {
+//            String knightId = String.format("%02dKnight", i);
+//            createCharacter(CHARACTER + knightId + ".atlas","deal",i);
+//        }
+    }
+    private Image createCharacterImage(String knightId, String animationName) {
+        TextureAtlas atlas = MainGame.getAsM().get(CHARACTER + knightId + ".atlas", TextureAtlas.class);
+        Array<TextureAtlas.AtlasRegion> idleFrames = atlas.findRegions(animationName);
+        Animation<TextureRegion> anim = new Animation<>(0.1f, idleFrames, Animation.PlayMode.LOOP);
+        return new Image(anim.getKeyFrame(0)) {
+            float stateTime = 0f;
+            @Override
+            public void act(float delta) {
+                super.act(delta);
+                stateTime += delta;
+                setDrawable(new TextureRegionDrawable(anim.getKeyFrame(stateTime, true)));
+            }
+        };
+    }
+    private String getKnightStats(String knightId) {
+        // Lấy dữ liệu thực từ JSON/class của bạn, ở đây demo tạm
+        if (knightId.equals("01Knight")) return "Máu: 120\nTấn công: 20\nThủ: 10";
+        if (knightId.equals("02Knight")) return "Máu: 100\nTấn công: 25\nThủ: 8";
+        // ... tiếp tục các knight khác
+        return "Máu: 110\nTấn công: 22\nThủ: 9";
+    }
+    private void showNextKnight() {
+        currentKnightIndex = (currentKnightIndex + 1) % knightIds.size;
+        updateKnightDisplay();
+    }
+    private void showPrevKnight() {
+        currentKnightIndex = (currentKnightIndex - 1 + knightIds.size) % knightIds.size;
+        updateKnightDisplay();
+    }
+    private void updateKnightDisplay() {
+        // Xoá actor animation cũ
+        knightAnimImage.remove();
+        knightAnimImage = createCharacterImage(knightIds.get(currentKnightIndex), "idle");
+        knightAnimImage.setSize(screenHeight * 0.7f, screenHeight * 0.7f);
+        knightAnimImage.setPosition((screenWidth - knightAnimImage.getWidth()) / 2f, screenHeight * 0.3f);
+        knightAnimImage.debug();
+        rootGroup.addActor(knightAnimImage);
+
+        // Update label tên
+        knightNameLabel.setText(knightNames[currentKnightIndex]);
+        // Update label stats/mô tả
+        knightStatsLabel.setText(getKnightStats(knightIds.get(currentKnightIndex)));
+    }
     private void createBackground() {
         String namePopup = "popup_000";
         TextureRegion skill = MainGame.getAsM().getRegion(UI_WOOD, "popup_000");
         UIImage popup = new UIImage(skill).name(namePopup).parent(rootGroup).bounds(0,0,screenWidth,screenHeight).debug(true);
 
-        UIButton button = new UIButton(MainGame.getAsM().getRegion(UI_WOOD, "next_up_005"),MainGame.getAsM().getRegion(UI_WOOD, "next_down_006"))
-            .parent(rootGroup).size(screenHeight*0.2f,screenHeight*0.2f).pos(screenWidth*0.9f,screenHeight*0.4f)
-            .onClick(()->{})
-            .debug(true);
+//        UIButton button = new UIButton(MainGame.getAsM().getRegion(UI_WOOD, "next_up_005"),MainGame.getAsM().getRegion(UI_WOOD, "next_down_006"))
+//            .parent(rootGroup).size(screenHeight*0.2f,screenHeight*0.2f).pos(screenWidth*0.9f,screenHeight*0.4f)
+//            .onClick(()->{})
+//            .debug(true);
 
     }
 
--- core/src/main/java/com/game/utils/data/SaveGameECS.java
+++ core/src/main/java/com/game/utils/data/SaveGameECS.java
@@ -2,8 +2,32 @@ package com.game.utils.data;
 
 import com.badlogic.gdx.utils.Array;
 
+/**
+ * Root object chứa toàn bộ trạng thái động của game ở thời điểm lưu (save).
+ * Lưu danh sách entity (player, npc, enemy, item...), map hiện tại, meta, version...
+ */
 public class SaveGameECS {
-    public Array<EntitySaveData> entities; // mỗi entity là 1 nhân vật, quái, v.v.
-    public String currentMap;
-    public long saveTime;
+    // Phiên bản save, dùng để migrate/convert khi cập nhật game (optional, nên có)
+    public String version = "1.0";
+
+    // Thời gian lưu, có thể dùng System.currentTimeMillis() hoặc TimeUtils.millis()
+    public long saveTime = 0;
+
+    // Map hiện tại hoặc màn chơi hiện tại
+    public String currentMapId;
+
+    // Toàn bộ entity động đang tồn tại trong game
+    public Array<EntitySaveData> entities = new Array<>();
+
+    // Nếu cần, có thể lưu thêm một số trạng thái phụ (setting, slot, ...)
+
+    // Constructor trống cho (de)serialization
+    public SaveGameECS() {}
+
+    // Constructor nhanh nếu muốn
+    public SaveGameECS(String currentMapId, Array<EntitySaveData> entities) {
+        this.currentMapId = currentMapId;
+        this.entities = entities;
+        this.saveTime = System.currentTimeMillis();
+    }
 }
